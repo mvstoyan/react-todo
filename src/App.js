dd0bae9f91666e9b5874be859d921086b36a497a
Marina Tilton <m.v.stoyan@gmail.com>
2023-09-24 18:10:47 -0700

    lesson_1_1 React todoList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 function App() {
+  // Array of tasks
+  const todoList = [
+    { id: 1, title: "Complete assignment" },
+    { id: 2, title: "Read 6 chapters/sections in the Road to React" },
+    { id: 3, title: "Watch a video explanation for these chapters of the book" },
+    { id: 4, title: "Submit the homework assignment submission form" },
+  ];
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {/* render the list */}
+        {todoList.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
