d95caec4871c0fc067886228d61b26d6511599b6
Marina Tilton <m.v.stoyan@gmail.com>
2023-09-27 14:05:57 -0700

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -3,25 +3,11 @@ import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
 function App() {
-  // Array of tasks
-  const todoList = [
-    { id: 1, title: "Complete assignment" },
-    { id: 2, title: "Read 6 chapters/sections in the Road to React" },
-    { id: 3, title: "Watch a video explanation for these chapters of the book" },
-    { id: 4, title: "Submit the homework assignment submission form" },
-  ];
-
   return (
     <div>
       <h1>Todo List</h1>
       <AddTodoForm />
       <TodoList />
-      <ul>
-        {/* render the list */}
-        {todoList.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
     </div>
   );
 }
