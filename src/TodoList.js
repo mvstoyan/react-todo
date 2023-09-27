296ba0d4e1381da825d1c0c63cdd3e7cb295a75b
Marina Tilton <m.v.stoyan@gmail.com>
2023-09-27 13:51:20 -0700

    todoList outside the function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-function TodoList() {
-  // Array of tasks
-  const todoList = [
-    { id: 1, title: "Complete assignment" },
-    { id: 2, title: "Read 6 chapters/sections in the Road to React" },
-    { id: 3, title: "Watch a video explanation for these chapters of the book" },
-    { id: 4, title: "Submit the homework assignment submission form" },
-  ];
+// Array of tasks
+const todoList = [
+  { id: 1, title: "Complete assignment" },
+  { id: 2, title: "Read 6 chapters/sections in the Road to React" },
+  { id: 3, title: "Watch a video explanation for these chapters of the book" },
+  { id: 4, title: "Submit the homework assignment submission form" },
+];
 
+function TodoList() {
   return (
     <ul>
       {/* render the list */}
