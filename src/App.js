3019d598145326acefce31d2816e92d25615acab
Marina Tilton <m.v.stoyan@gmail.com>
2023-10-09 19:23:27 -0700

    Fetch Data from Airtable/GET

--- src/App.js
+++ src/App.js
@@ -2,35 +2,39 @@ import React from "react";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
 
-// OLD CODE - saving the todo after page reload
-// function useSemiPersistentState() {
-//  const [todoList, setTodoList] = React.useState(
-//    JSON.parse(localStorage.getItem("savedTodoList")) || []
-//  );
-//  React.useEffect(() => {
-//    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-//  }, [todoList]);
-//  return [todoList, setTodoList];
-//}
 function App() {
   const [todoList, setTodoList] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const airtableUrl = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}`;
+  const airtableToken = process.env.REACT_APP_AIRTABLE_API_TOKEN;
+
+  const fetchData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${airtableToken}`,
+      },
+    };
+    try {
+      const response = await fetch(airtableUrl, options);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const todosFromAPI = await response.json();
+      const todos = todosFromAPI.records.map((todo) => {
+        return { id: todo.id, title: todo.fields.title };
+      });
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   React.useEffect(() => {
-    new Promise((resolve, reject) =>
-      setTimeout(
-        () =>
-          resolve({
-            data: {
-              todoList: JSON.parse(localStorage.getItem("savedTodoList")),
-            },
-          }),
-        2000
-      )
-    ).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   React.useEffect(() => {
