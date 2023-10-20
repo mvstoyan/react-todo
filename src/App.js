e4387669b50b93ad093bfccd02dc2d3663d2c9f8
Marina Tilton <m.v.stoyan@gmail.com>
2023-10-19 19:38:50 -0700

    navbar, Home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import TodoContainer from "./components/TodoContainer/TodoContainer";
-import "./public/globalStyles.css";
+import Home from "./components/Home/Home";
+import style from "./public/globalStyles.module.css"; 
 
 function App() {
   return (
     <BrowserRouter>
+      <nav className={style.navbar}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/todo">Todo List</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
-        <Route exact path="/" element={<TodoContainer />} />
-        <Route path="/new" element={<h1>New Todo List</h1>} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="/todo" element={<TodoContainer />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={style.container}>
+      <h1 className={style.heading}>Welcome to the Home Page</h1>
+    </div>
+  );
+};
+
+export default Home;
